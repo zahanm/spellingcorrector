4670e759b023e685c383ca1d34717fe2e1758485
Zahan Malkani <zahanm@gmail.com>
2012-04-18 16:07:33 -0700

    scaffolding of query data reading in

--- corrector.py
+++ corrector.py
@@ -1,7 +1,25 @@
 
 import sys
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz -'
+queries_loc = './data/dev/queries.txt'
+gold_loc = './data/dev/gold.txt'
+
+alphabet = "abcdefghijklmnopqrstuvwxyz0123546789&$+_' "
+
+def read_query_data():
+  queries = []
+  correct = []
+  google = []
+  with open(queries_loc) as f:
+    for line in f:
+      queries.append(line.rstrip())
+  with open(gold_loc) as f:
+    for line in f:
+      a, b = line.rstrip().split('\t')
+      correct.append(a)
+      google.append(b)
+  assert( len(queries) == len(correct) and len(correct) == len(google) )
+  return (queries, correct, google)
 
 if __name__ == '__main__':
   print(sys.argv)
