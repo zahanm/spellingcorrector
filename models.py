bc57736b63c7322fbf19bfd187ca9187761ee4d0
Zahan Malkani <zahanm@gmail.com>
2012-04-18 14:27:15 -0700

    read in training data gzipped

--- models.py
+++ models.py
@@ -1,25 +1,25 @@
 
 import sys
-import os
 import os.path
+import gzip
+from glob import iglob
 
 training_corpus_loc = './data/corpus/'
-edit1s_loc = './data/edit1s.txt'
+edit1s_loc = './data/edit1s.txt.gz'
 
 def scan_corpus():
   """
   Scans through the training corpus and counts how many lines of text there are
   """
-  for sub_dir in os.listdir( training_corpus_loc ):
-    print >> sys.stderr, 'processing dir: ' + sub_dir
-    for fname in os.listdir( os.path.join( training_corpus_loc, sub_dir ) ):
-      with open( os.path.join( training_corpus_loc, sub_dir, fname ) ) as f:
-        num_lines = 0
-        for line in f:
-          # remember to remove the trailing \n
-          line = line.rstrip()
-          num_lines += 1
-        print >> sys.stderr, 'Number of lines in ' + fname + ' is ' + str(num_lines)
+  for block_fname in iglob( os.path.join( training_corpus_loc, '*.*' ) ):
+    print >> sys.stderr, 'processing dir: ' + block_fname
+    with gzip.open( block_fname ) as f:
+      num_lines = 0
+      for line in f:
+        # remember to remove the trailing \n
+        line = line.rstrip()
+        num_lines += 1
+      print >> sys.stderr, 'Number of lines in ' + block_fname + ' is ' + str(num_lines)
 
 def read_edit1s():
   """
@@ -27,7 +27,7 @@ def read_edit1s():
   It's a list of tuples, structured as [ .. , (misspelled query, correct query), .. ]
   """
   edit1s = []
-  with open(edit1s_loc) as f:
+  with gzip.open(edit1s_loc) as f:
     # the .rstrip() is needed to remove the \n that is stupidly included in the line
     edit1s = [ line.rstrip().split('\t') for line in f if line.rstrip() ]
   return edit1s
