1d37e63c0745bf2d18b9c60788340f46e9bce044
Zahan Malkani <zahanm@gmail.com>
2012-04-20 09:10:50 -0700

    marshal in comments

--- toy_code.py
+++ toy_code.py
@@ -38,7 +38,9 @@ def serialize_data(data, fname):
 def unserialize_data(fname):
   """
   Reads a pickled data structure from a file named `fname` and returns it
-  IMPORTANT: Only call pickle.load( .. ) on a file that was written to using pickle.dump( .. )
+  IMPORTANT: Only call marshal.load( .. ) on a file that was written to using marshal.dump( .. )
+  marshal has a whole bunch of brittle caveats you can take a look at in teh documentation
+  It is faster than everything else by several orders of magnitude though
   """
   with open(fname, 'rb') as f:
     return marshal.load(f)
